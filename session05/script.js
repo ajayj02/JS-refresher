27aee3979137a3f95a0a85a5541769adbda12269
Ajay Joshi <joshiajay311@gmail.com>
2024-06-24 19:16:58 +0530

    event propagation

--- session05/script.js
+++ session05/script.js
@@ -1,26 +1,13 @@
-/*
-Debouncing code snippet
+const div = document.querySelector("div");
+const form = document.querySelector("form");
+const button = document.querySelector("button");
 
-let counter = 0;
-function getData() {
-  // calls an API and gets Data
-  console.log("fetching data..", counter++);
+div.addEventListener("click", trigger);
+
+form.addEventListener("click", trigger);
+
+button.addEventListener("click", trigger);
+
+function trigger(event) {
+  alert("currentTarget = " +  event.currentTarget.tagName + " target = " + event.target.tagName);
 }
-
-function newFn(fn, d) {
-  let timer;
-
-  return function () {
-    clearTimeout(timer);
-    timer = setTimeout(()=>{
-        fn()
-    }, d);
-  };
-}
-
-const debouncedFn = newFn(getData, 800);
-
-*/
-
-//Throttling
-
