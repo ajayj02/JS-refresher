936ca419fcbc9e04fb03672791664154a9fe437f
Ajay Joshi <joshiajay311@gmail.com>
2024-08-24 18:19:51 +0530

    java basics

---
+++ Java/session-01/Hello.java
@@ -0,0 +1,26 @@
+// import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String args[]) {
+        int nums[][] = new int[4][];
+
+        nums[0] = new int[2];
+        nums[1] = new int[3];
+        nums[2] = new int[4];
+        nums[3] = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++)
+                nums[i][j] = (int) (Math.random() * 100);
+        }
+
+        for (int n[] : nums) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
